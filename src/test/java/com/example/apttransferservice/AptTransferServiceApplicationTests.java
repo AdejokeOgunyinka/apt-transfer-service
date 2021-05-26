227b226264483856704cab7f199d0807ba7f2cf0
Adejoke <ogunyinkajoke@gmail.com>
2021-05-26 11:20:54 +0100

    created folder structure using spring initializr for the springboot pp

---
+++ src/test/java/com/example/apttransferservice/AptTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apttransferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
